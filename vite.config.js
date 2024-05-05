76e33081683d2fad3826e9306aaf5a6cc1c37eb8
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2024-05-05 07:28:18 +0500

    updating

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[Sameer-khan0]/[react_portfolio]/", // Adjust accordingly
+  base: "/vite-deploy/", // Adjust accordingly
 })
